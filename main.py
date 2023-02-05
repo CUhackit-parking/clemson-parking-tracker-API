25fbd25bcc14a1468d59b3cce392aee2f6d58acf
John Turner <johnpturner8@gmail.com>
2023-02-04 22:01:02 -0500

    Get map, increment, and decrement

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import json
 
 app = FastAPI()
-counter = 0
 
-@app.get("/increment")
-def increment():
-    global counter
-    counter += 1
-    return {"message": "counter incremented", "value": counter}
+@app.patch("/increment/{lot_name}")
+async def increment(lot_name):
+    f = open('map.geojson', 'r+')
+    map = json.load(f)
+    features = map.get("features")
 
+    for feature in features:
+        if(feature["properties"]["name"] == lot_name):
+            feature["properties"]["count"] += 1
+            json_object = json.dumps(map, indent=4)
+            f.seek(0)
+            f.truncate()
+            f.write(json_object)
+            f.close()
+            return feature["properties"]
+    f.close()
+    raise HTTPException(status_code=404, detail="Parking lot not found")
+    
 
-@app.get("/decrement")
-def decrement():
-    global counter
-    counter -= 1
-    return {"message": "counter decremented", "value": counter}
+@app.patch("/decrement/{lot_name}")
+async def increment(lot_name):
+    f = open('map.geojson', 'r+')
+    map = json.load(f)
+    features = map.get("features")
 
+    for feature in features:
+        if(feature["properties"]["name"] == lot_name):
+            feature["properties"]["count"] -= 1
+            json_object = json.dumps(map, indent=4)
+            f.seek(0)
+            f.truncate()
+            f.write(json_object)
+            f.close()
+            return feature["properties"]
+    f.close()
+    raise HTTPException(status_code=404, detail="Parking lot not found")
 
-
-
+@app.get("/")
+def getMap():
+    f = open('map.geojson')
+    map = json.load(f)
+    f.close()
+    return map
