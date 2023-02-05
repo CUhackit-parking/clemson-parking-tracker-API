469409a40de4b4ca1815a4c1f73549538c0b768e
John Turner <johnpturner8@gmail.com>
2023-02-05 01:27:38 -0500

    Add endpoints using deta

--- main.py
+++ main.py
@@ -1,48 +1,80 @@
+import traceback
 from fastapi import FastAPI, HTTPException
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 import json
+from deta import Deta
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as e:
+        err = traceback.format_exception(type(e), e, e.__traceback__)
+        return JSONResponse(content={"traceback": err}, status_code=500)
 
 app = FastAPI()
+# app.middleware('http')(catch_exceptions_middleware)
 
-@app.patch("/increment/{lot_name}")
+deta = Deta('b01e4uo1_AKg3ZAFjdQkFwrUKxYPwmPRohk85egpb') # configure your Deta project
+db = deta.Base('simpleDB')
+db2 = deta.Base('simpleDB')
+
+@app.patch("/lot/{lot_name}/increment/")
 async def increment(lot_name):
-    f = open('map.geojson', 'r+')
-    map = json.load(f)
-    features = map.get("features")
+    updates = {
+        "count": db.util.increment()
+    }
+    return db.update(updates, lot_name)
 
-    for feature in features:
-        if(feature["properties"]["name"] == lot_name):
-            feature["properties"]["count"] += 1
-            json_object = json.dumps(map, indent=4)
-            f.seek(0)
-            f.truncate()
-            f.write(json_object)
-            f.close()
-            return feature["properties"]
-    f.close()
-    raise HTTPException(status_code=404, detail="Parking lot not found")
-    
-
-@app.patch("/decrement/{lot_name}")
+@app.patch("/lot/{lot_name}/decrement/")
 async def increment(lot_name):
-    f = open('map.geojson', 'r+')
-    map = json.load(f)
-    features = map.get("features")
+    updates = {
+        "count": db.util.increment()
+    }
+    return db.update(updates, lot_name)
 
-    for feature in features:
-        if(feature["properties"]["name"] == lot_name):
-            feature["properties"]["count"] -= 1
-            json_object = json.dumps(map, indent=4)
-            f.seek(0)
-            f.truncate()
-            f.write(json_object)
-            f.close()
-            return feature["properties"]
-    f.close()
-    raise HTTPException(status_code=404, detail="Parking lot not found")
+@app.get("/lot/{lot_name}")
+def getLot(lot_name):
+    lot = db.get(lot_name)
+    return lot
+
+@app.post("/lot/{lot_name}")
+def createLot(lot_name):
+    lot = db.put({
+        "name": lot_name,
+        "count": 0,
+    }, lot_name)
+    return lot
+
+@app.delete("/lot/{lot_name}")
+def createLot(lot_name):
+    lot = db.delete(lot_name)
+    return lot
 
 @app.get("/")
 def getMap():
-    f = open('map.geojson')
+    f = open('map.geojson', 'r')
     map = json.load(f)
+    features = map.get("features")
+
+    for feature in features:
+        feature['properties'] = db.get(feature['properties']['name'])
+    
     f.close()
     return map
+
+@app.post("/reset/")
+def reset():
+    print(db.fetch()._items)
+    for item in db.fetch()._items:
+        db.delete(item.name)
+
+    f = open('map.geojson', 'r')
+    map = json.load(f)
+    features = map.get("features")
+
+    for feature in features:
+        db.put(feature['properties'], feature['properties']['name'])
+        
+    f.close()
+    return db.fetch()
